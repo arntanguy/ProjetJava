58a074bf45b0be7ff9f537c62f492b6edbb6dce2
fauvelja <fauvelja@polytech.unice.fr>
2012-01-24 12:20:01 +0100

    Vehicule

---
+++ src/objets/Bus.java
@@ -0,0 +1,24 @@
+package objets;
+/**
+*
+*@author 
+*@version 1.0
+*/
+public class Bus extends Vehicule{
+	
+    /**
+     *Contructeur par défaut. Permet de construire un Vehicule avec un id.
+     *@param
+     *@return
+     */
+    public Bus(){
+	super(50);
+    }
+    /**Méthode qui permet d'afficher un Vehicule
+     *@param
+     *@return String : toString
+     */
+    public String toString() {
+	return "Vehicule n°" + getIdVehicule() + "\n" + "Capacité : " + getCap();
+    }
+}
